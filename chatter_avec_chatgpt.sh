99a955406949ae4b02f25cbd261424a1494d053f
pirate du bato <anonyme201504@gmail.com>
2024-08-12 23:07:14 +0200

    hey

--- chatter_avec_chatgpt.sh
+++ chatter_avec_chatgpt.sh
@@ -1,1 +1,1 @@
-pyhon3 -m pip nstall ---default-timeout=100 python-dotenv  openai asyncio torch  transformers
+python3 -m pip install --default-timeout=100 python-dotenv  openai asyncio torch  transformers
