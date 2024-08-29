60eb2fc2228844192ae51e30d82c46ea77406203
pirate du bato <anonyme201504@gmail.com>
2024-08-29 23:43:47 +0200

    accelarete

--- chatter_avec_chatgpt.sh
+++ chatter_avec_chatgpt.sh
@@ -1,2 +1,2 @@
-python3.9 -m pip install --default-timeout=100 python-dotenv  openai asyncio torch  transformers
+python3.9 -m pip install --default-timeout=100 python-dotenv  openai asyncio torch  transformers accelerate
 python3.9 -m pip install pillow --upgrade
